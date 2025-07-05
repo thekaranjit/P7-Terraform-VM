11d24656653ca0dc0db9cfa23da5e246438e41e0
Karan Singh <thekaranjit@outlook.com>
2025-07-06 01:34:43 +0530

    Added Windows VM Settings

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "azurerm_resource_group" "example" {
 
 # Virtual Network Resource
 resource "azurerm_virtual_network" "main" {
-  name                = "terraformlab-vnet2"
+  name                = "terraformlab-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
@@ -34,7 +34,7 @@ resource "azurerm_subnet" "example2" {
 
 # Public IP Resource
 resource "azurerm_public_ip" "example" {
-  name                = "terraformlab-pip2"
+  name                = "terraformlab-pip"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   allocation_method   = "Static"
@@ -47,12 +47,12 @@ resource "azurerm_public_ip" "example" {
 
 # Network Interface Resource
 resource "azurerm_network_interface" "example" {
-  name                = "terraformlab-nic2"
+  name                = "terraformlab-nic"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
   ip_configuration {
-    name                          = "testconfiguration2"
+    name                          = "testconfiguration"
     subnet_id                     = azurerm_subnet.example1.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.example.id
@@ -65,7 +65,7 @@ resource "azurerm_network_interface" "example" {
 
 # Network Security Group Resource
 resource "azurerm_network_security_group" "example" {
-  name                = "terraformlab-nsg2"
+  name                = "terraformlab-nsg"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
@@ -96,11 +96,11 @@ resource "azurerm_network_interface_security_group_association" "example" {
 # Virtual Machine Resource
 
 resource "azurerm_virtual_machine" "main" {
-  name                  = "terraformlab-vm2"
+  name                  = "terraformlab-vm"
   location              = azurerm_resource_group.example.location
   resource_group_name   = azurerm_resource_group.example.name
   network_interface_ids = [azurerm_network_interface.example.id]
-  vm_size               = "Standard_DS1_v4"
+  vm_size               = "Standard_BS1_v4"
 
   # Uncomment this line to delete the OS disk automatically when deleting the VM
   # delete_os_disk_on_termination = true
@@ -109,25 +109,36 @@ resource "azurerm_virtual_machine" "main" {
   # delete_data_disks_on_termination = true
 
   storage_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
     version   = "latest"
   }
   storage_os_disk {
     name              = "myosdisk1"
     caching           = "ReadWrite"
     create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
+    managed_disk_type = "StandardSSD_LRS"
   }
   os_profile {
     computer_name  = "hostname"
     admin_username = "testadmin"
     admin_password = "Password1234!"
   }
-  os_profile_linux_config {
-    disable_password_authentication = false
+
+# OS Profile Config for Windows
+
+ os_profile_windows_config {
+    provision_vm_agent = true
+    enable_automatic_upgrades = true
   }
+  
+#OS Configuration for Linux
+# Uncomment this block to configure Linux VM settings
+
+#   os_profile_linux_config {
+#     disable_password_authentication = false
+#   }
   tags = {
     environment = "Lab"
   }
